30b669b96edcc53eac7464ab9c4992a78ed1da3a
Kamil Raczka <kamil.raczka700@gmail.com>
2020-03-11 09:53:20 +0100

    building UI with logic on button

--- backlogs/lib/screens/BacklogScreen.dart
+++ backlogs/lib/screens/BacklogScreen.dart
@@ -2,26 +2,45 @@ import 'package:backlogs/models/Item.dart';
 import 'package:backlogs/utilities/colors.dart';
 import 'package:flutter/material.dart';
 
-class BacklogScreen extends StatelessWidget {
-  var items = <Item>[Item('test', true)];
+class BacklogScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _BacklogScreenState();
+}
+
+class _BacklogScreenState extends State<BacklogScreen> {
+  var items = <Item>[
+    Item('first item', true),
+    Item('second item', true),
+    Item('third item', true),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: CustomScrollView(
         slivers: <Widget>[
           SliverAppBar(
-            expandedHeight: 256.0,
-            title: Text('SliverAppBar text'),
+            expandedHeight: 196.0,
+            title: Text('All tasks'),
+            backgroundColor: ColorsLibrary.primaryColor,
           ),
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-                (context, index) => _buildTask(items.first),
-                childCount: 20),
+          SliverPadding(
+            padding: const EdgeInsets.all(24.0),
+            sliver: SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (context, index) {
+                  for (var item in items) {
+                    _buildTaskRow(item);
+                  }
+                },
+                childCount: items.length,
+              ),
+            ),
           ),
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: null,
+        onPressed: () => _onFabPressed(),
         backgroundColor: ColorsLibrary.primaryColor,
         child: Icon(
           Icons.add,
@@ -30,15 +49,48 @@ class BacklogScreen extends StatelessWidget {
     );
   }
 
-  Widget _buildTask(Item item) {
-    return Row(
-      children: <Widget>[
-        Text(item.description),
-        Checkbox(
-          value: item.isDone,
-          onChanged: null,
-        )
-      ],
+  Widget _buildTaskRow(Item item) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Text(
+              item.description,
+            ),
+          ),
+          Checkbox(
+            value: item.isDone,
+            onChanged: null,
+          )
+        ],
+      ),
     );
+  }
+
+  void _onFabPressed() {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            ListTile(
+              title: Text('Test tile'),
+              onTap: () {
+                _createTask("createTaskMock");
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _createTask(String text) {
+    setState(() {
+      items.add(Item(text, false));
+    });
   }
 }
--- backlogs/lib/utilities/colors.dart
+++ backlogs/lib/utilities/colors.dart
@@ -2,6 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:tinycolor/tinycolor.dart';
 
 class ColorsLibrary {
-  static Color primaryColor = TinyColor.fromString('#5B74FA').color;
+  static Color primaryColor = TinyColor.fromString('#5879FF').color;
   static Color textColor = TinyColor.fromString('#4d4e50').color;
 }
