df71790339e80cbe9ddbacafe4c829e8b09916ce
Kamil Raczka <kamil.raczka700@gmail.com>
2020-03-28 13:17:04 +0100

    updated colors

--- backlogs/lib/utilities/colors_library.dart
+++ backlogs/lib/utilities/colors_library.dart
@@ -3,5 +3,16 @@ import 'package:tinycolor/tinycolor.dart';
 
 class ColorsLibrary {
   static Color primaryColor = TinyColor.fromString('#5879FF').color;
-  static Color textColor = TinyColor.fromString('#4d4e50').color;
+  static Color backgroundColor = TinyColor.fromString('##FCFCFC').color;
+  static Color textColorBold = TinyColor.fromString('#333333').color;
+  static Color textColorMedium = TinyColor.fromString('#7A7A7A').color;
+  static Color textColorLight = TinyColor.fromString('##9A9A9A').color;
+  static Color textColorBoldInversed = TinyColor.fromString('#FFFFFF').color;
+  static Color accentColor1 = TinyColor.fromString('#FFBA67').color;
+  static Color accentColor2 = TinyColor.fromString('#FF8D77').color;
+  static Color accentColor3 = TinyColor.fromString('#00D376').color;
+  static Color accentColor4 = TinyColor.fromString('#927ED6').color;
+  static Color accentColor5 = TinyColor.fromString('#EA614F').color;
+  static Color accentColor6 = TinyColor.fromString('#B858C9').color;
+  static Color accentColor7 = TinyColor.fromString('#00B5C2').color;
 }
